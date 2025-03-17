3d4e36ba562175660f2f848c39fde0d5731b4fb0
Nishantjain10 <nishant.jain.775@gmail.com>
2025-03-17 16:54:32 +0530

    config: setup VuePress with Appwrite theme configuration

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { getDirname, path } from '@vuepress/utils'
+import customTheme from './theme/index.js'
+
+const __dirname = getDirname(import.meta.url)
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'Appwrite DevKit',
+  description: 'Vue-powered Static Site Generator',
+
+  theme: customTheme({
+    logo: '/images/appwrite-light.svg',
+    logoDark: '/images/appwrite-dark.svg',
+    
+    colorMode: {
+      defaultMode: 'light',
+    },
+
+    navbar: [
+      { text: 'Home', link: '/' },
+      { text: 'Get Started', link: '/get-started' },
+      { 
+        text: 'Appwrite', 
+        link: 'https://appwrite.io',
+      },
+    ],
+
+    repo: 'appwrite/appwrite',
+    docsRepo: 'appwrite/docs',
+    docsBranch: 'main',
+    editLink: true,
+  }),
+
+  bundler: viteBundler(),
+})
